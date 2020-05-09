d6310c295c125ff3d6651176ab30aa8bfb687a0b
gebogebogebo <gebogebogeboge@gmail.com>
2020-05-09 11:35:53 +0900

    update

---
+++ bin/JPKIPINTool.bat
@@ -0,0 +1,3 @@
+echo off
+powershell -NoProfile -ExecutionPolicy Unrestricted .\%~n0.ps1
+Timeout 5
---
+++ bin/JPKIPINTool.ps1
@@ -0,0 +1,27 @@
+﻿Set-Location (Split-Path ( & { $myInvocation.ScriptName } ) -parent)
+Add-Type -Path ".\JPKIReader.dll";
+
+Write-Host "カードリーダーにマイナンバーカードが乗っているかどうかチェックします"
+
+$result = [JPKIReaderLib.JPKIReader]::IsJPKICardExist()
+
+if( $result -eq $false ) {
+    Write-Host "- マイナンバーカードが認識できませんでした"
+    return
+}
+Write-Host "- マイナンバーカード認識OK!"
+Write-Host "---"
+
+
+Write-Host "利用者証明用パスワード（4桁の数字）　いわゆる　認証 PINのリトライ回数をチェックします"
+$result = [JPKIReaderLib.JPKIReader]::GetAuthenticationPINRetryCount()
+$message = "- PINリトライ回数 = " + $result + "回"
+Write-Host $message
+Write-Host "---"
+
+Write-Host "署名用パスワード（半角6文字-16文字）　いわゆる　署名 PINのリトライ回数をチェックします"
+$result = [JPKIReaderLib.JPKIReader]::GetSignaturePINRetryCount()
+$message = "- PINリトライ回数 = " + $result + "回"
+Write-Host $message
+Write-Host "---"
+
